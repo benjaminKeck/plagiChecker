1fd39a752e3b30a6cbbc4d03ee0a8a46b9a44fb3
benjaminKeck <benjamin_keckes@web.de>
2013-04-07 16:44:25 +0200

    Char Iterator

---
+++ plagiCheck/src/hm/edu/algo/plagiCheck/kAux/CharIterator.java
@@ -0,0 +1,30 @@
+package hm.edu.algo.plagiCheck.kAux;
+
+import java.util.Iterator;
+
+public class CharIterator implements Iterator<Character>{
+
+	private final String str;
+	private int pos;
+	
+	public CharIterator(String str){
+		this.str = str;
+		pos=0;
+	}
+	@Override
+	public boolean hasNext() {
+		return pos < str.length();
+	}
+
+	@Override
+	public Character next() {
+		return str.charAt(pos++);
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
