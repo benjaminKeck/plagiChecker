ad490b09bdcaa6dc14a3ac972aff4b516d6f33c2
benjaminKeck <benjamin_keckes@web.de>
2013-04-26 14:03:26 +0200

    enum hinzugefügt

--- plagiCheck/src/hm/edu/algo/plagiCheck/kAux/ITokenizer.java
+++ plagiCheck/src/hm/edu/algo/plagiCheck/kAux/ITokenizer.java
@@ -3,6 +3,10 @@ package hm.edu.algo.plagiCheck.kAux;
 import hm.edu.algo.plagiCheck.triePackage.ITrie;
 
 public interface ITokenizer {
+	
+	public enum TokenTypes{
+		ID, DATE, WHITESPACE, KOMMA, PERIOD;
+	}
 	public void setFile(String file);
 	public void setTrieAndAction(ITrie trie, IActionAtInsert action);
 	public void start();
