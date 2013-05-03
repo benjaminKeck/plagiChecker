c24f784fc3c811b861dbf34d6660ba8ebfdb1e07
benjaminKeck <benjamin_keckes@web.de>
2013-05-03 12:44:14 +0200

    von mö

---
+++ plagiCheck/src/hm/edu/algo/plagiCheck/main/PlagiCheckerTippsVonMoencke.java
@@ -0,0 +1,21 @@
+package hm.edu.algo.plagiCheck.main;
+
+public class PlagiCheckerTippsVonMoencke {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		//ILexer lexer = new Filterlexer(new lexer(new BufferedReader(...), new DFA()));
+		/*
+		 * while(th != eofToken){
+		 	th = lexer.getToken();
+			
+			
+			
+			}
+		*/
+	}
+
+}
